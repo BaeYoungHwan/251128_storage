398eafdb69b7e20f19352047b19482db14bbfccc
BaeYoungWhan <uee8351773@naver.com>
2026-01-30 17:55:23 +0900

    260130 SQL수정 김포공항 > 서울역

--- docs/3. SQL/251219 CreateTable.sql
+++ docs/3. SQL/251219 CreateTable.sql
@@ -58,7 +58,7 @@ i.local_small + i.local_middle + i.local_large as "Total",
 Round((((i.local_small + i.local_middle + i.local_large) - (u.able_small + u.able_middle + u.able_large))::numeric / (i.local_small + i.local_middle + i.local_large)) * 100)::text || '%' as "percent",
 observed_at, u.created_at
 from "storage".locker_usage u, "storage".locker_inventory i
-where u.local_id = i.local_id and u.local_name like '%김포공항%'
+where u.local_id = i.local_id and u.local_name like '%서울역%'
 order by observed_at desc, percent desc ;
 
 select * from "storage"."local";
